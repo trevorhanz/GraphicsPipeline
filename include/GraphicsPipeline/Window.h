87cbc5b845bc01afad0b790956d5a4597fd8b196
Trevor Hanz <trevorhanz@gmail.com>
2020-06-20 17:09:56 -0500

    Fixed doxygen config

--- include/GraphicsPipeline/Window.h
+++ include/GraphicsPipeline/Window.h
@@ -15,6 +15,8 @@
 * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ************************************************************************/
 
+//! \file Window.h
+
 #ifndef __GP_WINDOW_H__
 #define __GP_WINDOW_H__
 
