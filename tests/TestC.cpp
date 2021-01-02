e24dbfdc89dff36daa3b06dc931d65b042004c2f
Trevor Hanz <trevorhanz@gmail.com>
2021-01-02 12:33:06 -0600

    Added basic unit tests

---
+++ tests/TestC.cpp
@@ -0,0 +1,39 @@
+/************************************************************************
+* Copyright (C) 2021 Trevor Hanz
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+************************************************************************/
+
+#include <GraphicsPipeline/GP.h>
+
+#include "gtest/gtest.h"
+
+TEST(C, Init)
+{
+  gp_system* system = gp_system_new();
+  EXPECT_NE(0, (uintptr_t)system);
+  
+  gp_context* context = gp_system_context_new(system);
+  EXPECT_NE(0, (uintptr_t)context);
+  
+  gp_context_free(context);
+  
+  gp_system_free(system);
+}
+
+int main(int argc, char* argv[])
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
